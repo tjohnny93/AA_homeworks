854913344d8e60658f99f667dd1f440f449f6f50
Johnny <tjohnny93@berkeley.edu>
2019-05-20 00:59:22 -0700

    Finish

--- W2D1/lib/simon.rb
+++ W2D1/lib/simon.rb
@@ -42,23 +42,34 @@ class Simon
   end
 
   def require_sequence
-    
+    puts "Repeat the sequence by entering the first letter of each color on a new line."
+    @seq.each do |color|
+      use_color = gets.chomp
+      if color[0] != user_color
+        @game_over = true
+        break
+      end
+    end
+    sleep 0.25
 
   end
 
   def add_random_color
+    @seq << COLORS.shuffle[0]
 
   end
 
   def round_success_message
-
+    "Round successful. Here's the next sequence:"
   end
 
   def game_over_message
-
+    puts "Game over! You made it #{@sequence_length - 1} rounds."
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 end
