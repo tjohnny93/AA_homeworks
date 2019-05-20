624a9ee5fc0cfe9ab72ead79356125c5bdb8b4b1
Johnny <tjohnny93@berkeley.edu>
2019-05-19 23:28:26 -0700

    Up to sequence

--- W2D1/lib/simon.rb
+++ W2D1/lib/simon.rb
@@ -4,23 +4,45 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-    @sequence_length
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
 
   end
 
   def play
-
+    until @game_over
+      take_turn
+      system("clear")
+    end
+    game_over_message
+    reset_game
   end
 
   def take_turn
+    show_sequence
+    require_sequence
+
+    unless @game_over
+      round_success_message
+      @sequence_length += 1
+    end
 
   end
 
   def show_sequence
+    add_random_color
+    @seq.each do |color|
+      puts color
+      sleep 0.75
+      system("clear")
+      sleep 0.25
+    end
 
   end
 
   def require_sequence
+    
 
   end
 
