b101d2b63c995f8fb84962d46c507b1f775ef7de
Johnny <tjohnny93@berkeley.edu>
2019-05-24 07:32:32 -0700

    Finish HW W2D5

--- W2D5/LRU_Cache.rb
+++ W2D5/LRU_Cache.rb
@@ -1,17 +1,35 @@
   class LRUCache
     def initialize
+        @size = size
+        @cache = []
+
     end
 
     def count
       # returns number of elements currently in cache
+      @cache.count
     end
 
     def add(el)
       # adds element to cache according to LRU principle
+      if @cache.include?(ele)
+        @cache.delete(ele)
+        @cache << ele
+      elsif count >= @size
+        @cache.shift
+        @cache << ele
+      else
+        @cache << ele
+      end
+
+
+
     end
 
     def show
       # shows the items in the cache, with the LRU item first
+      p @cache
+      nil
     end
 
     private
